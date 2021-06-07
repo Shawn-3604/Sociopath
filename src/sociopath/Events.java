0f25d1e6878a8747fe007f04f6922b851e8662d1
Shawn-3604 <maizat13501@gmail.com>
2021-06-08 04:06:21 +0800

    Events 1&2 done

--- src/sociopath/Events.java
+++ src/sociopath/Events.java
@@ -11,38 +11,69 @@ package sociopath;
  */
 
 /**
- * P_1:the person that seeking for help
- * P_2:the person that helping
+ * A:the person that seeking for help
+ * B:the person that helping
  * Grade: to show whether P_2 can teach or not
  * answer: to decide whether to help or not
  */
 public class Events {
-    public static void Events1(Person P_1,Person P_2,boolean Grade,boolean answer){
+    /**
+    * A:the person that seeking for help
+    * B:the person that helping
+    * Grade: to show whether P_2 can teach or not
+    * answer: to decide whether to help or not
+    */
+    public static void Events1(Person A,Person B,boolean Grade,boolean answer){
         System.out.println("Event 1: ");
-        if(P_1.contain(P_2)||P_2.contain(P_1)){
-            System.out.println(P_1.getName()+" and "+P_2.getName()
+        if(A.contain(B)||B.contain(A)){
+            System.out.println(A.getName()+" and "+B.getName()
                     +" are not strangers");
         }else{
-            System.out.println(P_1.getName()+" asks "+P_2.getName()
+            System.out.println(A.getName()+" asks "+B.getName()
                     +" to help to teach how to solve the Data Structure course's lab question");
             if(answer){
                 if(Grade){
-                    P_2.setRep(P_1, 10);
-                    System.out.println(P_1.getName()+" said that the learning experience with "
-                            +P_2.getName()+" is very pleasant");
-                    System.out.println(P_2.getName()+">>"+P_1.getName()+",rep: "
-                            +P_2.getRep(P_1));
+                    B.setRep(A, 10);
+                    System.out.println(A.getName()+" said that the learning experience with "
+                            +B.getName()+" is very pleasant");
+                    System.out.println(B.getName()+">>"+A.getName()+",rep: "
+                            +B.getRep(A));
                 }else{
-                    P_2.setRep(P_1, 2);
-                    System.out.println(P_1.getName()+" said that the learning experience with "
-                            +P_2.getName()+" is not very pleasant");
-                    System.out.println(P_2.getName()+">>"+P_1.getName()+",rep: "
-                            +P_2.getRep(P_1));
+                    B.setRep(A, 2);
+                    System.out.println(A.getName()+" said that the learning experience with "
+                            +B.getName()+" is not very pleasant");
+                    System.out.println(B.getName()+">>"+A.getName()+",rep: "
+                            +B.getRep(A));
                 }
             }else{
-                System.out.println(P_1.getName()+" refuse to help "+P_2.getName());
+                System.out.println(A.getName()+" refuse to help "+B.getName());
             }
         }
     }
-        
+    
+    
+    public static void Events2(Person A,Person B,Person C,boolean side){
+        System.out.println("Event 2: ");
+        double rep;
+        if(A.contain(B)||B.contain(A)){
+            if(side){
+                rep=A.getRep(B)*0.5;
+                C.addRep(A,rep);
+                System.out.println(B.getName()+" speak about "
+                            +A.getName()+"'s good side with "+C.getName());
+                    System.out.println(C.getName()+">>"+A.getName()+",rep: "
+                            +C.getRep(A));
+            }else{
+                rep=A.getRep(B)*(-1);
+                C.addRep(A, rep);
+                System.out.println(B.getName()+" speak about "
+                            +A.getName()+"'s bad side with "+C.getName());
+                    System.out.println(C.getName()+">>"+A.getName()+",rep: "
+                            +C.getRep(A));
+            }
+        }else{
+            System.out.println(A.getName()+" and "+B.getName()
+                    +" are strangers");
+        }
+    }
 }
